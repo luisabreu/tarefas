bcbc909418e7898767ef6ae7ef82ac37c1e0ccd5
Luis Abreu <luisabreu@gov-madeira.pt>
2015-11-17 11:59:40 +0000

    Adicao de motor de views Jade
    * Criacao de uma vista simples para exemplificar o uso deste motor

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var express = require("express");
 
 var app = express();//singleton da nossa app
 
+//definicao motor de vista
+app.set("view engine", "jade");
+
 app.get("/", (req, res) =>{
-   res.send("<html><body><h1>Olá de express</h1></body></html>")
+   //res.send("<html><body><h1>Olá de express</h1></body></html>")
+    res.render("index", {titulo: "Olá de Express!"});
 });
 
 app.get("/api/users", (req, res) => {
